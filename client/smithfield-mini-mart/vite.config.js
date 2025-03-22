8acb32d493be841a2336afdcd62bad88ef44fb30
Ashray <soaresashray@yahoo.com>
2025-03-22 12:50:31 -0500

    configure Vite build settings for production output directory

--- client/smithfield-mini-mart/vite.config.js
+++ client/smithfield-mini-mart/vite.config.js
@@ -7,4 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),
   ],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 })
